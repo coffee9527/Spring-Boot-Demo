f6934b9e6c435ae3563937fc232ba233e40d821d
coffee9527 <2488223984@qq.com>
2018-05-15 12:43:27 +0800

    测试提交

--- common-service-api/src/main/java/top/pancras/entity/Computer.java
+++ common-service-api/src/main/java/top/pancras/entity/Computer.java
@@ -4,4 +4,43 @@ package top.pancras.entity;
  * Created by Administrator on 2018/4/14 0014.
  */
 public class Computer {
+    //机型
+    private String type;
+    //cpu信息
+    private String cpu;
+    //容量
+    private int content;
+
+    public Computer() {
+    }
+
+    public Computer(String type, String cpu, int content) {
+        this.type = type;
+        this.cpu = cpu;
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getContent() {
+        return content;
+    }
+
+    public void setContent(int content) {
+        this.content = content;
+    }
 }
--- common-service-api/src/main/java/top/pancras/service/ComputerService.java
+++ common-service-api/src/main/java/top/pancras/service/ComputerService.java
@@ -1,7 +1,22 @@
 package top.pancras.service;
 
+import org.springframework.web.bind.annotation.*;
+import top.pancras.entity.Computer;
+
 /**
  * Created by Administrator on 2018/4/14 0014.
  */
+@RequestMapping("/computer")
 public interface ComputerService {
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    String info();
+
+    @RequestMapping(value = "/getName", method = RequestMethod.GET)
+    String getName(@RequestParam("name") String name);
+
+    @RequestMapping(value = "/more", method = RequestMethod.GET)
+    Computer more(@RequestHeader("name") String name, @RequestHeader("cpu") String cpu, @RequestHeader("content") int content);
+
+    @RequestMapping(value = "/detail", method = RequestMethod.POST)
+    String detail(@RequestBody Computer computer);
 }
